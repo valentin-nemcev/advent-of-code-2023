67c3a6a0976e2b47a1042647b01c99e69a87590f
Valentin Nemcev <valentin.nemcev@smartly.io>
2023-12-25 16:05:28 +0200

    day 1 full

--- index.ts
+++ index.ts
@@ -19,35 +19,73 @@ async function fetchInput(day: number) {
   return file.text();
 }
 
+const add = (a: number, b: number) => a + b;
+
 describe("Day 1", () => {
-  function calibrationValue(line: string) {
-    const matchDigit = (c: string) => ("1" <= c && c <= "9" ? Number(c) : 0);
+  const digitWords = [
+    "one",
+    "two",
+    "three",
+    "four",
+    "five",
+    "six",
+    "seven",
+    "eight",
+    "nine",
+  ];
 
+  const matchDigit = (dir: "left" | "right", s: string, i: number) => {
+    const c = dir == "left" ? s[i] : s[s.length - i - 1];
+    return "1" <= c && c <= "9" ? Number(c) : 0;
+  };
+
+  const matchWordDigit = (dir: "left" | "right", s: string, i: number) =>
+    matchDigit(dir, s, i) ||
+    digitWords.findIndex((w) =>
+      dir == "left" ? s.startsWith(w, i) : s.endsWith(w, s.length - i)
+    ) + 1;
+
+  const calibrationValue = (match: typeof matchDigit) => (line: string) => {
     let left = 0;
-    for (let i = 0; i < line.length; i++)
-      if ((left = matchDigit(line[i]) ?? left)) break;
-
     let right = 0;
-    for (let i = line.length - 1; i >= 0; i--)
-      if ((right = matchDigit(line[i]) ?? right)) break;
-
+    for (let i = 0; i < line.length; i++) {
+      left ||= match("left", line, i);
+      right ||= match("right", line, i);
+      if (left && right) break;
+    }
     return left * 10 + right;
-  }
-
-  function part1(input: string) {
-    const lines = input.split("\n");
-    return lines.map(calibrationValue).reduce((a, b) => a + b);
-  }
+  };
 
   test("*", async () => {
     const example = `1abc2
-pqr3stu8vwx
-a1b2c3d4e5f
-treb7uchet`;
+  pqr3stu8vwx
+  a1b2c3d4e5f
+  treb7uchet`;
 
-    expect(example.split("\n").map(calibrationValue)).toEqual([12, 38, 15, 77]);
-    expect(part1(example)).toEqual(142);
+    const perLine = (s: string) =>
+      s.split("\n").map(calibrationValue(matchDigit));
 
-    expect(part1(await fetchInput(1))).toBe(55108);
+    expect(perLine(example)).toEqual([12, 38, 15, 77]);
+    expect(perLine(example).reduce(add)).toEqual(142);
+
+    expect(perLine(await fetchInput(1)).reduce(add)).toBe(55108);
+  });
+
+  test("**", async () => {
+    const example = `two1nine
+  eightwothree
+  abcone2threexyz
+  xtwone3four
+  4nineeightseven2
+  zoneight234
+  7pqrstsixteen`;
+
+    const perLine = (s: string) =>
+      s.split("\n").map(calibrationValue(matchWordDigit));
+
+    expect(perLine(example)).toEqual([29, 83, 13, 24, 42, 14, 76]);
+    expect(perLine(example).reduce(add)).toEqual(281);
+
+    expect(perLine(await fetchInput(1)).reduce(add)).toBe(56324);
   });
 });
